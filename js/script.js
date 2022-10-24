116982de9d8a802a0864204aa930741fe40d6d14
Matthéo <dasilvagomesmattheo@gmail.com>
2022-10-24 15:04:56 +0200

    Ex1 Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-//dasilvagomes-mattheo-dm-tech
+//dasilvagomes-mattheo-dm-tech-test
+
+function reverseString(chaine) {
+
+    return chaine.split("").reverse().join("");
+    }
+    
+    var inversion = reverseString("Inverse");
+
+console.log(inversion);
