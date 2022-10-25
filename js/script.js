a98985eb3d5d8bb63fa07207ef053f73b2f0b3de
Matthéo <dasilvagomesmattheo@gmail.com>
2022-10-25 18:28:08 +0200

    Ex4 Commit V2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 //dasilvagomes-mattheo-dm-tech
 
-let debut = 5;
-for (let cpt = debut +1; cpt < debut +11; cpt++) {
-    //J'affiche dans ma console la somme des chiffres/nombres de la valeur rentrée
-    console.log(cpt);
-  }
+//J'attribue une valeur de départ de 1 qui me servira à faire mon calcul
+//La valeur de fin est la valeur que nous voulons changer pour arreter la boucle
+let debut = 1;
+let fin = 5;
+let somme = 0;
+//Tant que la valeur du début est plus petite ou égale à la valeur de fin, alors la boucle continue
+while (debut <= fin) {
+  //J'ajoutes à ma somme la valeur de début soit +1
+  somme += debut;
+  //Tant que la boucle n'est pas terminée, j'ajoutes à ma valeur de début +1 donc elle passe à 2 etc..
+  debut += 1;
+}
+console.log(somme);
